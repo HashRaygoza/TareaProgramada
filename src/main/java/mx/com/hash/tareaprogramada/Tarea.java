1db629dbd9772cf290ece4147ded5340c04b2deb
david <david@localhost>
2018-10-05 23:30:50 -0500

    Version 1.0
    Ejemplo sencillo del uso de Timer y TimerTask para crear una tarea
    programada

---
+++ src/main/java/mx/com/hash/tareaprogramada/Tarea.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.hash.tareaprogramada;
+
+import java.util.TimerTask;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author david
+ */
+public class Tarea extends TimerTask {
+    static private final Logger LOGGER = Logger.getLogger("mx.com.hash.tareaprogramada.Tarea");
+    private Integer contador;    
+    
+    public Tarea() {
+        contador = 0;
+    }
+
+    @Override
+    public void run() {
+        LOGGER.log(Level.INFO, "Numero de ejecución {0}", contador);
+        contador++;
+    }
+    
+}
---
+++ src/main/java/mx/com/hash/tareaprogramada/TareaProgramada.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.com.hash.tareaprogramada;
+
+import java.util.Timer;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author david
+ */
+public class TareaProgramada {
+    static private final Logger LOGGER = Logger.getLogger("mx.com.hash.tareaprogramada.TareaProgramada");
+    
+    static public void main(String[] args){
+        Tarea tarea = new Tarea();
+        Timer temporizador = new Timer();
+        Integer segundos = 5;
+        
+        temporizador.scheduleAtFixedRate(tarea, 0, 1000*segundos);
+    }
+}
